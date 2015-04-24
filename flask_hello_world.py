22c140db789f5d2fb59e155f4354d3ce5dbaf9b9
Kevin Koshy <kevin@logicmash.com>
2015-04-24 19:19:06 +0300

    My Second Commit

--- flask_hello_world.py
+++ flask_hello_world.py
@@ -8,6 +8,24 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello World'
 
+@app.route('/test/<search_query>')
+def search(search_query):
+    return search_query
+
+@app.route("/integer/<int:value>")
+def int_type(value):
+    print value + 1
+    return "correct"
+
+@app.route("/float/<float:value>")
+def float_type(value):
+    print value+1
+    return "correct"
+
+@app.route("/path/<path:value>")
+def path_type(value):
+    print value
+    return "correct"
 
 if __name__ == '__main__':
     app.run()
